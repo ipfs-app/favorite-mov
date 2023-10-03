45914f35fff684808e9c299e2cd9b05c535217f5
bill080307 <bill080307@live.cn>
2023-10-03 23:49:45 +0800

    页头配置

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,56 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-// import { create } from 'ipfs-core';
-// import { createOrbitDB } from '@orbitdb/core';
+import { create } from 'ipfs-core';
+import { createOrbitDB,Documents } from '@orbitdb/core';
 
 const pElementRef = ref(null)
+
+import cheader from './component/header.vue'
+// import loading from './component/loading.vue'
+// import genre from './component/genre.vue'
+// import lists from './component/lists.vue'
+// import player from './component/player.vue'
+// import comment from './component/comment.vue'
+// import donate from './component/donate.vue'
+import cfooter from './component/footer.vue'
+import axios from 'axios'
 
 onMounted(async () => {
   // const ipfs = await create();
   // const orbitdb = await createOrbitDB({ ipfs });
-  // const db = await orbitdb.open('my-db')
-  // pElementRef.value.textContent = db.address;
+  // const db = await orbitdb.open('vod', { Database: Documents({ indexBy: 'name'} ) })
+  // console.log(db.address)
   // await db.close();
   // await ipfs.stop();
+  axios.get("/config.json").then((res) => {
+    if (!localStorage.getItem("username")) {
+      localStorage.setItem("username", res.data.username)
+    }
+    if (localStorage.getItem("fdbs")) {
+      let dbs = JSON.parse(localStorage.getItem("fdbs"))
+      for (let i = 0; i < res.data.fdbs.length; i++) {
+        let f = true
+        for (let j = 0; j < dbs.length; j++) {
+          if (dbs[j].title === res.data.fdbs[i].title) {
+            f = false
+            break
+          }
+        }
+        if (f) {
+          dbs.push(res.data.fdbs[i])
+        }
+      }
+      localStorage.setItem("fdbs", JSON.stringify(dbs))
+    } else {
+      localStorage.setItem("fdbs", JSON.stringify(res.data.fdbs))
+    }
+  });
 })
-import cheader from './component/header.vue'
-import loading from './component/loading.vue'
-import genre from './component/genre.vue'
-import lists from './component/lists.vue'
-import player from './component/player.vue'
-import comment from './component/comment.vue'
-import donate from './component/donate.vue'
-import cfooter from './component/footer.vue'
 </script>
 
 <template>
   <cheader></cheader>
-  <div class="container">
+  <!-- <div class="container">
     <loading></loading>
     <genre></genre>
     <lists></lists>
@@ -36,6 +61,6 @@ import cfooter from './component/footer.vue'
       <div class="col-6"><comment></comment></div>
     </div>
     <donate></donate>
-  </div>
+  </div> -->
   <cfooter></cfooter>
 </template>
--- src/component/header.vue
+++ src/component/header.vue
@@ -1,6 +1,3 @@
-<script setup>
-</script>
-
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
     <div class="container-fluid">
@@ -12,34 +9,61 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link disabled" href="#">数据库：</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+          <li class="nav-item" v-for="db in nav_db" >
+            <a class="nav-link" href="#">{{ db.title }}</a>
           </li>
         </ul>
         <form class="d-flex">
-          <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#configModal">Config</button>
+          <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal"
+            data-bs-target="#configModal">Config</button>
         </form>
       </div>
     </div>
   </nav>
-<div class="modal fade" id="configModal" tabindex="-1" aria-labelledby="configModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="configModalLabel">站点配置</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">保存</button>
+  <div class="modal fade" id="configModal" tabindex="-1" aria-labelledby="configModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="configModalLabel">站点配置</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="username">username</span>
+            <input type="text" class="form-control" aria-label="Username" aria-describedby="username" v-model="username">
+          </div>
+          <div class="mb-3">
+            <label for="dbs" class="form-label">数据库:</label>
+            <textarea class="form-control" id="dbs" rows="8" v-model="dbs"></textarea>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" @click="save_config" data-bs-dismiss="modal">保存</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
-<style>
-</style>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const username = ref("anonymous")
+const dbs = ref("[]")
+const nav_db = ref([])
+onMounted(() => {
+  const configModal = document.getElementById('configModal')
+  nav_db.value = JSON.parse(localStorage.getItem("fdbs"))
+  configModal.addEventListener('shown.bs.modal', () => {
+    username.value = localStorage.getItem("username")
+    dbs.value = JSON.stringify(JSON.parse(localStorage.getItem("fdbs")), null, 2)
+  })
+
+})
+function save_config() {
+  localStorage.setItem("username", username.value)
+  localStorage.setItem("fdbs", JSON.stringify(JSON.parse(dbs.value)))
+}
+</script>
+<style></style>
